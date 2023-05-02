c1a40b1455317518864c70434afe9941ab35b406
KovacsZoltanDaniel <kovacs.zoltan@student.ms.sapientia.ro>
2023-05-02 11:41:25 +0300

    lab_10

---
+++ lab_10/main.c
@@ -0,0 +1,19 @@
+#include "bst_person.h"
+
+int main() {
+   Node *root = NULL;
+   int n;
+   if(!freopen("person.txt", "r",stdin)){
+       printf(FILE_OPENING_ERROR_MESSAGE);
+       exit(FILE_OPENING_ERROR_CODE);
+   }
+    scanf("%i", &n);
+    for (int i = 0; i < n; ++i) {
+        Person x;
+        scanf("%s%i%i%i%i", x.nev, &x.data.year, &x.data.month, &x.data.day, &x.gender);
+        root = insert(root,x);
+    }
+    inorderTraversal(root);
+    freopen("CON", "r", stdin);
+    return 0;
+}
