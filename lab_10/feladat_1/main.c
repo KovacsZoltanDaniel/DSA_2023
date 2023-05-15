536ed96ea78606151a402e9a385dbae32ca1b3c4
KovacsZoltanDaniel <kovacs.zoltan@student.ms.sapientia.ro>
2023-05-15 13:49:55 +0300

    lab_10_1

--- lab_10/feladat_1/main.c
+++ lab_10/feladat_1/main.c
@@ -4,10 +4,8 @@ int main() {
     Node *root = createNewNode(1);
     insertLeft(root,2);
     insertRight(root,3);
-    Node * temp = insertLeft(root->left,4);
+    insertLeft(root->left,4);
     insertRight(root->left,5);
-    insertLeft(temp,8);
-    insertRight(temp,9);
     insertLeft(root->right,6);
     insertRight(root->right,7);
     ///traversals
